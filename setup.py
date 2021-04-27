39b81cddba42a21236f255786d36de4c0e580a3c
Yoshikawa Hiroyuki <yoh@cesmba.local>
2021-04-27 17:56:15 +0900

    [WIP] modify setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
-from distutils.core import setup
+import os, sys
+from distutils.core import setup, Extension
+
+import pybind11
+include_dirs = [pybind11.get_include()]
+library_dirs = []
+
+ex_module = Extension(
+    'filter_exmod',
+    sources=['pySPH/filter/filter_exmod.cpp'],
+    libraries=[],
+    include_dirs=include_dirs,
+    library_dirs=library_dirs,
+    extra_compile_args=['-std=c++14', '-fPIC'],
+)
+
 setup(name="pySPH",
-    version="0.2",
-    description="Python interface for SPH file of Sphere framework",
-    requires=["numpy", "pybind11"],
-    author="YOSHIKAWA Hiroyuki, FUJITSU LTD.",
-    author_email="yoh@fujitsu.com",
-    packages=["pySPH"],
+      version="0.2",
+      description="Python interface for SPH file of Sphere framework",
+      requires=["numpy", "pybind11"],
+      author="YOSHIKAWA Hiroyuki, FUJITSU LTD.",
+      author_email="yoh@fujitsu.com",
+      packages=["pySPH", "pySPH.filter"],
+      ext_modules=[ex_module]
     )
 
